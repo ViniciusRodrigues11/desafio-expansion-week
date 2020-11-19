7d88f64712c843282eab5f4e25d4b6433ea27798
Vinicius Rodrigues da Silva <vinicius.silva@saolucas.edu.br>
2020-11-19 11:11:55 -0400

    animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactPlayer from 'react-player'
 import { FiCheckCircle } from 'react-icons/fi'
+import { Fade } from "react-awesome-reveal";
 
 import './App.css';
 
@@ -15,62 +16,79 @@ function App() {
     <>
       <div className="box-intro">
         <div className="first-content">
+          <Fade>
+            <img src={Creative01} alt="" />
+          </Fade>
 
-          <img src={Creative01} alt="" />
           <div>
+
             <h1>Criatividade</h1>
             <p>“Criatividade é a arte de conectar ideias.” Steve Jobs</p>
+
           </div>
         </div>
 
         <div className="second-content">
-          <div className="card">
-            <p>A criatividade, mais do que algo inato,
-            pode ser construída por meio de conhecimentos
-            sobre determinado assunto, resiliência e até as próprias
-            experiências profissionais. Esta talvez seja a “soft skill”
-            mais importantes para os momentos de
+          <Fade direction="left">
+            <div className="card">
+              <p>A criatividade, mais do que algo inato,
+              pode ser construída por meio de conhecimentos
+              sobre determinado assunto, resiliência e até as próprias
+              experiências profissionais. Esta talvez seja a “soft skill”
+              mais importantes para os momentos de
              crise vivenciados pelas empresas.</p>
-          </div>
-          <img src={Creative02} alt="" />
+            </div>
+          </Fade>
+          <Fade direction="right"><img src={Creative02} alt="" /></Fade>
+
+
+
         </div>
 
       </div>
 
       <div className="box-media" >
+        <Fade>
+          <div className="box-video">
+            <div className="video-filter"></div>
+            <ReactPlayer
 
-        <div className="box-video">
-          <div className="video-filter"></div>
-          <ReactPlayer
-
-            url={vid}
-            loop={true}
-            muted={true}
-            autoPlay={true}
-            controls={false}
-            playing={true}
-            width="auto"
-            height="45vh"
-          />
-        </div>
+              url={vid}
+              loop={true}
+              muted={true}
+              autoPlay={true}
+              controls={false}
+              playing={true}
+              width="auto"
+              height="45vh"
+            />
+          </div>
+        </Fade>
         <div className="box-steps">
-          <img src={Creative03} alt="" />
+          <Fade direction="up"><img src={Creative03} alt="" /></Fade>
 
+          <Fade>
           <div>
             <p><FiCheckCircle size={20} color="#fff" /> Mantenha a mente aberta</p>
             <p><FiCheckCircle size={20} color="#fff" /> Exercite seu cérebro</p>
             <p><FiCheckCircle size={20} color="#fff" /> Valorize suas ideias</p>
           </div>
+          </Fade>
 
         </div>
+
 
         <div className="footer-infos">
-          <p>“Em 2015, a criatividade era considerada como a 10ª habilidade
-          mais importante para o mercado de trabalho.
-          Desde então, ela galgou posições e hoje é considerada o
-          terceiro skill principal para quem busca se tornar um profissional bem-sucedido.”</p>
-          <img src={Creative04} alt="" />
+          <Fade>
+            <p>“Em 2015, a criatividade era considerada como a 10ª habilidade
+            mais importante para o mercado de trabalho.
+            Desde então, ela galgou posições e hoje é considerada o
+          terceiro skill principal para quem busca se tornar um profissional bem-sucedido.”</p></Fade>
+          <Fade direction="left">
+            <img src={Creative04} alt="" />
+          </Fade>
         </div>
+
       </div>
 
     </>
